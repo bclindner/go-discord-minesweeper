a5e1f69add261d5209c943b56977a939fd80c4b5
Brian C. Lindner <brian@bclindner.com>
2019-02-02 12:56:10 -0500

    Updated with more advanced features

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ This is a simple Discord bot that generates Minesweeper boards.
 
 # Usage
 
-Download the binary or compile the source, then put this in a `config.json` file
-in the same directory as the bot:
+Download the binary or compile the source, then put your bot token in a
+`config.json` file in the same directory as the bot:
 ```json
 {
   "token": "<YOUR_BOT_TOKEN>"
@@ -13,4 +13,20 @@ in the same directory as the bot:
 ```
 
 Launch the bot, and type `!minesweeper` anywhere the bot has access to, and it
-should spit out a perfectly workable Minesweeper board.
+should spit out a perfectly workable 10x10 Minesweeper board with 10 mines.
+
+# Advanced Usage
+
+You can request a custom grid by typing `!minesweeper <width> <height>
+<number-of-mines>`. It may refuse your request if the grid is too big, or if
+more mines have been requested than cells (resulting in a literally unusable
+grid).
+
+You can set override the default grid settings in the `config.json` file:
+```json
+{
+  "token": "<YOUR_BOT_TOKEN>",
+  "defaultGridSize": [<WIDTH>, <HEIGHT>],
+  "defaultMines": <NUMBEROFMINES>
+}
+```
--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 	"os/signal"
 	"encoding/json"
 	"io/ioutil"
+	"regexp"
+	"strconv"
 )
 
 var (
+	commandTriggerRegex = regexp.MustCompile(`^!minesweeper ?([0-9]+)? ?([0-9]+)? ?([0-9]+)?$`)
 	numbers = [...]string{
 		"||:zero:||",
 		"||:one:||",
@@ -22,10 +25,13 @@ var (
 		"||:eight:||",
 		"||:bomb:||",
 	}
+	cfg Config
 )
 
 type Config struct {
 	Token string `json:"token"`
+	DefaultGridSize [2]int `json:"defaultGridSize"`
+	DefaultMines int `json:"defaultMines"`
 }
 
 func main() {
@@ -35,10 +41,16 @@ func main() {
 		fmt.Println("Failed to read config file:", err)
 	}
 	// parse config
-	var cfg Config
 	err = json.Unmarshal(cfgfile, &cfg)
 	if err != nil {
 		fmt.Println("Failed to parse config file:", err)
+	}
+	if cfg.DefaultGridSize == [2]int{0, 0} {
+		cfg.DefaultGridSize = [2]int{10,10}
+	}
+	if cfg.DefaultMines == 0 {
+		cells := cfg.DefaultGridSize[0] * cfg.DefaultGridSize[1]
+		cfg.DefaultMines = cells / 10
 	}
 	// start bot
 	bot, err := discordgo.New("Bot "+cfg.Token)
@@ -59,16 +71,55 @@ func main() {
 }
 
 func sendGrid(bot *discordgo.Session, evt *discordgo.MessageCreate) {
-	if evt.Message.Content != "!minesweeper" {
+	groups := commandTriggerRegex.FindStringSubmatch(evt.Message.Content)
+	if groups == nil {
 		return
 	}
+	x := cfg.DefaultGridSize[0]
+	y := cfg.DefaultGridSize[1]
+	mines := cfg.DefaultMines
+	if groups[1] != "" && groups[2] != "" && groups[3] != "" {
+		var err error
+		x, err = strconv.Atoi(groups[1])
+		if err != nil {
+			fmt.Println("Error converting X string: ",err)
+		}
+		y, err = strconv.Atoi(groups[2])
+		if err != nil {
+			fmt.Println("Error converting Y string: ",err)
+		}
+		mines, err = strconv.Atoi(groups[3])
+		if err != nil {
+			fmt.Println("Error converting mines string: ",err)
+		}
+	} else if groups[1] != groups[2] || groups[2] != groups[3] || groups[3] != groups[1] {
+		fmt.Println("invalid input")
+		SendErrorMessage(bot, evt, "invalid input")
+	}
+	if x > 20 || y > 20 {
+		SendErrorMessage(bot, evt, "that board's too big!")
+	}
+	if mines > (x * y) {
+		SendErrorMessage(bot, evt, "you can't make a board with more mines than cells!")
+	}
 	fmt.Println("making a new board for",evt.Message.Author.Username+"#"+evt.Message.Author.Discriminator)
-	grid := NewMSGrid(10, 10)
-	grid.Populate(20)
+	grid := NewMSGrid(x, y)
+	grid.Populate(mines)
 	grid.updateMineCount()
-	_, err := bot.ChannelMessageSend(evt.Message.ChannelID,DiscordGrid(grid))
+	dgrid := DiscordGrid(grid)
+	if len(dgrid) > 2000 {
+		SendErrorMessage(bot, evt, "that board's too big!")
+	}
+	_, err := bot.ChannelMessageSend(evt.Message.ChannelID,dgrid)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error sending board:",err)
+	}
+}
+
+func SendErrorMessage(bot *discordgo.Session, evt *discordgo.MessageCreate, msg string) {
+	_, err := bot.ChannelMessageSend(evt.Message.ChannelID,fmt.Sprintf("<@%s>, %s",evt.Message.Author.ID, msg))
+	if err != nil {
+		fmt.Println("Error sending error message:",err)
 	}
 }
 
